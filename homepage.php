de6aae81fbfff9683b5fa912ae98c82f72c34b8b
Mees <334104@student.mboutrecht.nl>
2022-02-22 15:55:28 +0100

    Navbar + Logo Added

--- homepage.php
+++ homepage.php
@@ -4,9 +4,25 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Bitter</title>
+    <link rel="shortcut icon" href="img/BitterLogoTitle.png">
+    <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
-    
+<header>
+    <nav>
+        <ul>
+            <a href=" http://www.project-3.nl/homepage.php">
+            <img class="logo" src="img/BitterLogo.png   ">
+            </a>
+            <li><a href="http://www.project-3.nl/homepage.php">Home</a></li>
+            <li><a href="http://www.project-3.nl/archief.php">Beets</a></li>
+            <li><a href="http://www.project-3.nl/dashboard">About us</a></li>
+            <li><a href="http://www.project-3.nl/login.php">Login</a></li>
+            <li><a href="http://www.project-3.nl/login.php">Sign Up</a></li>
+
+        </ul>
+    </nav>
+</header> 
 </body>
 </html>
--- login.php
+++ login.php
@@ -4,9 +4,23 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
     <title>Document</title>
 </head>
 <body>
+<nav>
+        <ul>
+            <a href=" http://www.project-3.nl/homepage.php">
+            <img class="logo" src="img/BitterLogo.png   ">
+            </a>
+            <li><a href="http://www.project-3.nl/homepage.php">Home</a></li>
+            <li><a href="http://www.project-3.nl/archief.php">Beets</a></li>
+            <li><a href="http://www.project-3.nl/dashboard">About us</a></li>
+            <li><a href="http://www.project-3.nl/login.php">Login</a></li>
+            <li><a href="http://www.project-3.nl/login.php">Sign Up</a></li>
+
+        </ul>
+    </nav>
     
 </body>
 </html>

76d67029157c5c27b4fb65b08b5df68fc972a79d
Mees <334104@student.mboutrecht.nl>
2022-02-23 16:35:59 +0100

    Added Tweets and small bugfixes

--- homepage.php
+++ homepage.php
@@ -6,9 +6,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Bitter</title>
     <link rel="shortcut icon" href="img/BitterLogoTitle.png">
-    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/homepage.css">
 </head>
 <body>
+
+<div>
+<a class="twitter-timeline" href="https://twitter.com/TwitterDev?ref_src=twsrc%5Etfw">Tweets by TwitterDev</a> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8" class="twitter-script"></script>
+</div>
 <header>
     <nav>
         <ul>
@@ -17,10 +21,9 @@
             </a>
             <li><a href="http://www.project-3.nl/homepage.php">Home</a></li>
             <li><a href="http://www.project-3.nl/archief.php">Beets</a></li>
-            <li><a href="http://www.project-3.nl/dashboard">About us</a></li>
+            <li><a href="http://www.project-3.nl/dashboard.php">About us</a></li>
             <li><a href="http://www.project-3.nl/login.php">Login</a></li>
-            <li><a href="http://www.project-3.nl/login.php">Sign Up</a></li>
-
+            <li><a href="http://www.project-3.nl/signup.php">Sign Up</a></li>
         </ul>
     </nav>
 </header> 
---
+++ signup.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bitter</title>
+    <link rel="shortcut icon" href="img/BitterLogoTitle.png">
+    <link rel="stylesheet" href="css/signup.css">
+</head>
+<body>
+<header>
+    <nav>
+        <ul>
+            <a href=" http://www.project-3.nl/homepage.php">
+            <img class="logo" src="img/BitterLogo.png   ">
+            </a>
+            <li><a href="http://www.project-3.nl/homepage.php">Home</a></li>
+            <li><a href="http://www.project-3.nl/archief.php">Beets</a></li>
+            <li><a href="http://www.project-3.nl/dashboard.php">About us</a></li>
+            <li><a href="http://www.project-3.nl/login.php">Login</a></li>
+            <li><a href="http://www.project-3.nl/signup.php">Sign Up</a></li>
+        </ul>
+    </nav>
+</header> 
+</body>
+</html>

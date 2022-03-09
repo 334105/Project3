42783e2685f73c7c9c105528909f24177dd5b619
Mees <334104@student.mboutrecht.nl>
2022-03-09 16:46:10 +0100

    Homepage | Login mainly content added
    + Small fixes en verplaatsingen van andere pages en content

--- login.php
+++ login.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Bitter</title>
     <link rel="shortcut icon" href="img/BitterLogoTitle.png">
-    <link rel="stylesheet" href="css/signup.css">
+    <link rel="stylesheet" href="./login/login.css">
 </head>
 <body>
 <header>
@@ -17,11 +17,30 @@
             </a>
             <li><a href="http://www.project-3.nl/homepage.php">Home</a></li>
             <li><a href="http://www.project-3.nl/archief.php">Beets</a></li>
-            <li><a href="http://www.project-3.nl/dashboard.php">About us</a></li>
+            <li><a href="http://www.project-3.nl/dashboard.php">Dashboard</a></li>
             <li><a href="http://www.project-3.nl/login.php">Login</a></li>
             <li><a href="http://www.project-3.nl/signup.php">Sign Up</a></li>
         </ul>
     </nav>
 </header> 
+
+<h2>Login Form</h2>
+
+
+  <div class="container">
+    <label for="uname"><b>Username</b></label>
+    <input type="text" placeholder="Enter Username" name="uname" required>
+
+    <label for="psw"><b>Password</b></label>
+    <input type="password" placeholder="Enter Password" name="psw" required>
+        
+    <button type="submit">Login</button>
+    <label>
+      <input type="checkbox" checked="checked" name="remember"> Remember me
+    </label>
+  </div>
+
+  </div>
+</form>
 </body>
 </html>
--- signup.php
+++ signup.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Bitter</title>
     <link rel="shortcut icon" href="img/BitterLogoTitle.png">
-    <link rel="stylesheet" href="css/signup.css">
+    <link rel="stylesheet" href="./signup/signup.css">
 </head>
 <body>
 <header>
@@ -17,7 +17,7 @@
             </a>
             <li><a href="http://www.project-3.nl/homepage.php">Home</a></li>
             <li><a href="http://www.project-3.nl/archief.php">Beets</a></li>
-            <li><a href="http://www.project-3.nl/dashboard.php">About us</a></li>
+            <li><a href="http://www.project-3.nl/dashboard.php">Dashboard</a></li>
             <li><a href="http://www.project-3.nl/login.php">Login</a></li>
             <li><a href="http://www.project-3.nl/signup.php">Sign Up</a></li>
         </ul>

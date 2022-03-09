1485fb9439496a74e932d24828f04f0c222c8e11
Mees <334104@student.mboutrecht.nl>
2022-03-09 15:07:32 +0100

    Co-authored-by: Afghanishepperd <Afghanishepperd@users.noreply.github.com>

--- homepage.php
+++ homepage.php
@@ -6,26 +6,33 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Bitter</title>
     <link rel="shortcut icon" href="img/BitterLogoTitle.png">
-    <link rel="stylesheet" href="css/homepage.css">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    
+    <link rel="stylesheet" href="./css/style.css">
 </head>
 <body>
+    
+    <h1>Bitter Tweets</h1>
+    <header>
+        <nav>
+            <ul>
+                <a href=" http://www.project-3.nl/homepage.php">
+                <img class="logo" src="img/BitterLogo.png   ">
+                </a>
+                <li><a href="http://www.project-3.nl/homepage.php">Home</a></li>
+                <li><a href="http://www.project-3.nl/archief.php">Beets</a></li>
+                <li><a href="http://www.project-3.nl/dashboard.php">Dashboard</a></li>
+                <li><a href="http://www.project-3.nl/login.php">Login</a></li>
+                <li><a href="http://www.project-3.nl/signup.php">Sign up</a></li>
+            </ul>
+        </nav>
+    </header> 
 
-<div>
-<a class="twitter-timeline" href="https://twitter.com/TwitterDev?ref_src=twsrc%5Etfw">Tweets by TwitterDev</a> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8" class="twitter-script"></script>
-</div>
-<header>
-    <nav>
-        <ul>
-            <a href=" http://www.project-3.nl/homepage.php">
-            <img class="logo" src="img/BitterLogo.png   ">
-            </a>
-            <li><a href="http://www.project-3.nl/homepage.php">Home</a></li>
-            <li><a href="http://www.project-3.nl/archief.php">Beets</a></li>
-            <li><a href="http://www.project-3.nl/dashboard.php">About us</a></li>
-            <li><a href="http://www.project-3.nl/login.php">Login</a></li>
-            <li><a href="http://www.project-3.nl/signup.php">Sign Up</a></li>
-        </ul>
-    </nav>
-</header> 
-</body>
-</html>
+    <div class="box3">
+    <a class="twitter-timeline" href="https://twitter.com/TwitterDev?ref_src=twsrc%5Etfw">Tweets by TwitterDev</a> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8" class="twitter-script"></script>
+    </div>
+
+ </body>
+ </html>

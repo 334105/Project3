4c9673f7aa794c85d40430d307b9e8eec1849f26
Mees <334104@student.mboutrecht.nl>
2022-03-21 08:45:51 +0100

    scripts added + minor changes

---
+++ connect_db.php
@@ -0,0 +1,10 @@
+<?php
+   
+   define("SERVERNAME", "localhost");
+   define("USERNAME", "Admin");
+   define("PASSWORD", "mwhABoSIfsEqggkX");
+   define("DBNAME", "project3");
+
+
+    $conn = mysqli_connect(SERVERNAME, USERNAME, PASSWORD, DBNAME);
+?>
